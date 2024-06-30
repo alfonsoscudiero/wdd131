1af9f410658b04c5a09b895dc2fbb7ffe1f9429c
Alfonso <alfonsoscudiero@gmail.com>
2024-06-30 00:02:50 -0600

    update: js code

---
+++ scripts/place.js
@@ -0,0 +1,16 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+// Get the current year
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
+
+// Get the date and time the document was last modified
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last Modification: ${text}`;
