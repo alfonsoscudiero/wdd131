13916066ef0842cc4a95d6a539c91a3bc2871f48
Alfonso <alfonsoscudiero@gmail.com>
2024-07-20 23:56:09 -0600

    update: correct css code

--- scripts/web-project.js
+++ scripts/web-project.js
@@ -2,7 +2,7 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
-// Create a Responsive Menu - Add Event Listener
+// Responsive Navigation Menu - Add Event Listener
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
@@ -16,4 +16,52 @@ document.getElementById("year").innerHTML = year;
 // Get the date and time the document was last modified
 let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = `Last Modification: ${text}`;
+
+// Form Validation
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateEmail();
+    validateMessage();
+});
+
+function validateEmail() {
+    const emailValue = email.value.trim();
+    if (emailValue === '' || !isEmail(emailValue)) {
+        setError(email, 'Email is not valid');
+    } else {
+        setSuccess(email);
+    }
+}
+
+function validateMessage() {
+    const messageValue = message.value.trim();
+    if (messageValue === '') {
+        setError(message, 'Message cannot be empty');
+    } else {
+        setSuccess(message);
+    }
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\.,;:\s@"]+\.[^<>()[\]\.,;:\s@"]+)*\.[a-zA-Z]{2,}))$/.test(email);
+}
+
+function setError(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+
+
 
